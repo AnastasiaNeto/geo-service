f6e5d6ca34999c4a369d5bf2131f95b3a5395136
Anastasia <hagnum@mail.ru>
2022-11-20 18:03:37 +0500

    new test

---
+++ src/test/java/GeoServiceImplTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoServiceImpl;
+
+public class GeoServiceImplTest {
+    @Test
+    void byIpTest() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location locationRussian = geoService.byIp("172.323.43.12");
+        Location locationUSA = geoService.byIp("96.323.43.12");
+
+        Assertions.assertEquals(Country.RUSSIA, locationRussian.getCountry());
+        Assertions.assertEquals(Country.USA, locationUSA.getCountry());
+    }
+
+}
